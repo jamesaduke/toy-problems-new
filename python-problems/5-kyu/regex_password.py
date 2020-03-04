df96d803eafe4f04ab15a3cd296b694e2ccb58c3
James Aduke <jamesodwuor4@gmail.com>
2020-03-04 22:33:23 +0300

    refactoring

--- python-problems/5-kyu/regex_password.py
+++ python-problems/5-kyu/regex_password.py
@@ -7,6 +7,10 @@
 
 # Valid passwords will only be alphanumeric characters.
 
-
-
+# '(?=.*\d)'     # must contain one digit from 0-9
+#     '(?=.*[a-z])'  # must contain one lowercase characters
+#     '(?=.*[A-Z])'  # must contain one uppercase characters
+#     '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
+#     '{6,}'         # length at least 6 chars
+#     '$'            # end line
 regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"
