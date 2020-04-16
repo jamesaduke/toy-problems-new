efd5265005972262d2a1b9eb05531defaaecd9a7
James Aduke <jamesodwuor4@gmail.com>
2020-04-16 18:35:43 +0300

    two sum solution and title case modification

--- python-problems/6-kyu/title_case.py
+++ python-problems/6-kyu/title_case.py
@@ -1,16 +1,22 @@
-# A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+# A string is considered to be in title case if each word in the string is either (a) capitalised (that is,
+# only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into
+# lower case unless it is the first word, which is always capitalised.
 
-# Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+# Write a function that will convert a string into title case, given an optional list of exceptions (minor words).
+# The list of minor words will be given as a string with each word separated by a space. Your function should ignore
+# the case of the minor words string -- it should behave in the same way even if the case of the minor word string is
+# changed.
 
 # ###Arguments (Haskell)
 
-#     First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
-#     Second argument: the original string to be converted.
+# First argument: space-delimited list of minor words that must always be lowercase except for the first word in the
+# string. Second argument: the original string to be converted.
 
 # ###Arguments (Other languages)
 
-#     First argument (required): the original string to be converted.
-#     Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+# First argument (required): the original string to be converted. Second argument (optional): space-delimited list of
+# minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript
+# tests will pass undefined when this argument is unused.
 
 # ###Example
 
@@ -19,31 +25,31 @@
 # title_case('the quick brown fox') # should return: 'The Quick Brown Fox'
 
 
-def title_case(title, minor_words=''):
-    if title != '':
-        edited = []
-        title = [word.lower() for word in title.split()]
-        edited.append(title.pop(0).capitalize())
+# def title_case(title, minor_words=''):
+#     if title != '':
+#         edited = []
+#         title = [word.lower() for word in title.split()]
+#         edited.append(title.pop(0).capitalize())
+#
+#         if minor_words != None:
+#             minor_words = [word.lower() for word in minor_words.split()]
+#
+#             for word in title:
+#                 if word not in minor_words:
+#                     edited.append(word.capitalize)
+#                 else:
+#                     edited.append(word)
+#
+#         else:
+#             for word in title:
+#                 edited.append((word.capitalize()))
+#
+#         return ' '.join(edited)# gives this error TypeError: sequence item 0: expected str instance, bytes found
+#
+#     else:
+#         return title
 
-        if minor_words != None:
-            minor_words = [word.lower() for word in minor_words.split()]
-
-            for word in title:
-                if word not in minor_words:
-                    edited.append(word.capitalize)
-                else:
-                    edited.append(word)
-
-        else:
-            for word in title:
-                edited.append((word.capitalize()))
-
-        return ' '.join(edited)# gives this error TypeError: sequence item 0: expected str instance, bytes found
-
-    else:
-        return title
-
-    # clever solution
+# clever solution
 def title_case(title, minor_words=''):
     title = title.capitalize().split()
     minor_words = minor_words.lower().split()
