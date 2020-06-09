7e495054fd5d409da4705eaafb5593562a6e3c31
James Aduke <jamesodwuor4@gmail.com>
2020-06-09 14:35:44 +0300

    add staircase.py and min-max-sum.py

---
+++ python-problems/hackerrank-algos/min-max-sum.py
@@ -0,0 +1,13 @@
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of
+# the five integers. Then print the respective minimum and maximum values as a single line of two space-separated
+# long integers.
+#
+# For example,arr=[1,3,5,7,9] . Our minimum sum is 1+3+5+7 = 16 and our maximum sum is 3+5+7+9=24 . We would print
+# 18 24
+
+
+def miniMaxSum(arr):
+    sum=0
+    for i in range(len(arr)):
+        sum+=arr[i]
+    print ( sum-max(arr), sum-min(arr))
