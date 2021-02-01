08ea0a200a2b6aa336dbaf81f08156d7f061dda3
darkopscode <jamesodwuor4@gmail.com>
2021-02-01 16:02:41 +0300

    couple of changes

--- js/6-kyu/BinaryToTextAscii.js
+++ js/6-kyu/BinaryToTextAscii.js
@@ -12,9 +12,9 @@
 function binaryToString(binary) {
     let arr=[];
     for (let i=0;i<binary.length;i+=8){
-    arr.push(binary.slice(i,i+8))
+        arr.push(binary.slice(i,i+8));
     }
-    return arr.map(v=>String.fromCharCode(parseInt(v,2))).join('')
+    return arr.map(v=>String.fromCharCode(parseInt(v,2))).join('');
     
   }
   
